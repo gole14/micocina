654437e9f118269634873c6a8e93dc3a1edfb35f
Daniel Gomez <gole1407@gmail.com>
2016-10-04 15:16:26 -0500

    primer commit

---
+++ application/views/inicio.php
@@ -0,0 +1,44 @@
+<div class="container" id="main-container">
+	<div class="row" id="main-row">
+		<div class="row">
+			
+		</div>
+		   <div class="col-xs-12 col-sm-8 col-md-8">
+		   	<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+			  <ol class="carousel-indicators">
+			    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+			    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+			    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+			  </ol>
+			  <div class="carousel-inner" role="listbox">
+			    <div class="carousel-item active">
+			      <img src="assets/images/platillos/c1.JPG" alt="First slide">
+			    </div>
+			    <div class="carousel-item">
+			      <img src="assets/images/platillos/c2.jpg" alt="Second slide">
+			    </div>
+			    <div class="carousel-item">
+			      <img src="assets/images/platillos/c3.jpg" alt="Third slide">
+			    </div>
+			  </div>
+			  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+			    <span class="icon-prev" aria-hidden="true"></span>
+			    <span class="sr-only">Previous</span>
+			  </a>
+			  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+			    <span class="icon-next" aria-hidden="true"></span>
+			    <span class="sr-only">Next</span>
+			  </a>
+			</div>
+		   </div>
+		   <div class="col-xs-12 col-sm-4 col-md-4">
+		   	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		   	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		   	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		   	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		   	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		   	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		   </div>
+
+	</div>
+</div>
---
+++ assets/css/footer2.php
@@ -0,0 +1,76 @@
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<footer>
+	<div class="panel-group" id="accordion">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h4 class="panel-title">
+					<a data-toggle="collapse" href="#collapseOne" data-parent="#accordion">
+						Clic para ver las sucursales
+					</a>
+				</h4>
+			</div>
+			<div id="collapseOne" class="panel-collapse collapse">
+				<div class="panel-body">
+					<div class="col-lg-6 h-scroll">
+						<div class="row">
+							<div class="col-lg-2 col-md-2 col-sm-4 col-xs-6">
+								<a href="https://www.google.com.mx/maps/place/Mi+Cocina/@19.2721079,-103.7156654,19.54z/data=!4m12!1m6!3m5!1s0x84255ac535ba1e59:0x1ea8b3353502c516!2sMi+Cocina!8m2!3d19.2724469!4d-103.7157839!3m4!1s0x84255ac535ba1e59:0x1ea8b3353502c516!8m2!3d19.2724469!4d-103.7157839?hl=es-419">
+									<h3> Av. Venustiano Carranza </h3>
+								</a>
+								<ul>
+									<li> Tel. 332 6800 </li>
+									<li> Colima </li>
+								</ul>
+							</div>
+							<div class="col-lg-2 col-md-2 col-sm-4 col-xs-6">
+								<a href="https://www.google.com.mx/maps/place/Mi+Cocina/@19.2516285,-103.7109591,19.62z/data=!4m5!3m4!1s0x84255abc90ec48b9:0x8cb0ceb810a911d3!8m2!3d19.2518863!4d-103.710597?hl=es-419">
+				                    <h3> 
+				                    Av. Felipe Sevilla del Río 
+				                    </h3>
+				                </a>
+				                <ul>
+				                	<li> Tel. 313 6202</li>
+				                	<li> Colima </li>
+				                </ul>
+							</div>
+							<div class="col-lg-2 col-md-2 col-sm-4 col-xs-6">
+								<a href="https://www.google.com.mx/maps/place/Mi+Cocina/@19.2538929,-103.7449212,20.25z/data=!4m12!1m6!3m5!1s0x84255ab9b53bb9c9:0x5b8fdd1d218cd850!2sMi+Cocina!8m2!3d19.2579639!4d-103.7157649!3m4!1s0x0:0x60fea4a72e55ab8b!8m2!3d19.2540355!4d-103.7449063?hl=es-419">
+                    				<h3> 
+                    				Av. Benito Juárez
+                    				</h3>
+                    			</a>
+                    			<ul>
+                    				<li> Tel. 308 7666</li>
+                    				<li> Villa de Álvarez</li>
+                    			</ul>
+							</div>
+							<div class="col-lg-2 col-md-2 col-sm-4 col-xs-6">
+								<a href="https://www.google.com.mx/maps/place/Mi+Cocina/@19.2329055,-103.7210885,19.5z/data=!4m13!1m7!3m6!1s0x84255006eca0649d:0xf13cf537c0750142!2sGeneral+Pedro+Galv%C3%A1n+Sur+301,+San+Pablo,+28000+Colima,+Col.!3b1!8m2!3d19.2329093!4d-103.7213359!3m4!1s0x0:0x7dc94c742ef3735b!8m2!3d19.2327771!4d-103.7211406?hl=es-419">
+                    				<h3> 
+                    				Calzada Galván #301 
+                    				</h3>
+                   				 </a>
+                   				 <ul>
+                   				 	<li> Tel 312 1819</li>
+                   				 	<li>     312 0260</li>
+                   				 	<li> Colima </li>
+                   				 </ul>
+							</div>
+							<div class="col-lg-2 col-md-2 col-sm-4 col-xs-6">
+								<a href="https://www.google.com.mx/maps/place/Mi+Cocina/@19.2579163,-103.7158075,20.92z/data=!4m12!1m6!3m5!1s0x84255ab9b53bb9c9:0x5b8fdd1d218cd850!2sMi+Cocina!8m2!3d19.2579639!4d-103.7157649!3m4!1s0x84255ab9b53bb9c9:0x5b8fdd1d218cd850!8m2!3d19.2579639!4d-103.7157649?hl=es-419">
+                    				<h3> 
+                    				Av. Constitución 
+                    				</h3>
+                    			</a>
+                    			<ul>
+                    				<li> Tel. 323 4578</li>
+                    				<li> Colima </li>
+                    			</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
